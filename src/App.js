f06bf4413c09a78719554fca020aa8b41ac1ee2f
John Doe <johndoe@email.com>
2023-07-12 00:30:23 +0530

    helloji

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          hello sir
+          my name is janu
+          hello
+          hi
+          
         </p>
         <a
           className="App-link"
